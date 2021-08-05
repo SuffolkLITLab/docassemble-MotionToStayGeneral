5f97607a9385e87901b6af113a03f59b34ceeb6d
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-08-05 16:06:02 -0400

    Add dependencies, add attachment

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MotionToStayGeneral',
-      version='0.1.1',
+      version='0.1.2',
       description=('A docassemble extension.'),
       long_description='# docassemble.MotionToStayGeneral\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMia Bonardi, mbonardi@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -53,7 +53,7 @@ setup(name='docassemble.MotionToStayGeneral',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=[],
+      install_requires=['docassemble.ALAffidavitOfIndigency>=2.0.3', 'docassemble.AssemblyLine>=2.2.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MotionToStayGeneral/', package='docassemble.MotionToStayGeneral'),
      )
